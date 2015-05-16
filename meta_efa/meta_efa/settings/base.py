6e4c28ab1d88771eb363e07cc7fe67208fe75e7d
Arne Schauf <git@as-webservices.de>
2015-05-16 13:40:55 +0200

    add main and restapi to apps

--- meta_efa/meta_efa/settings/base.py
+++ meta_efa/meta_efa/settings/base.py
@@ -177,6 +177,8 @@ DJANGO_APPS = (
 
 # Apps specific for this project go here.
 LOCAL_APPS = (
+    'restapi',
+    'main',
 )
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps

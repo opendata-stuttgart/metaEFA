adf87e99b95a93c07f5ac8d9a4c72dc7d81a2c62
Arne Schauf <git@as-webservices.de>
2015-05-16 18:07:05 +0200

    disable pagination

--- meta_efa/meta_efa/settings/base.py
+++ meta_efa/meta_efa/settings/base.py
@@ -234,10 +234,10 @@ CELERY_ACCEPT_CONTENT = ['pickle']
 
 CELERY_IGNORE_RESULT = True
 
-REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 100,
-}
+# REST_FRAMEWORK = {
+#     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+#     'PAGE_SIZE': 100,
+# }
 
 # ######### CORS Headers https://github.com/ottoyiu/django-cors-headers/#configuration
 CORS_ORIGIN_WHITELIST = [

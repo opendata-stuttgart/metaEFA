9b0a0bf876cd304c9109fe809f5bc5d151aa2f0d
c1ko <ciko@shackspace.de>
2015-05-16 15:26:46 +0200

    Added searching

--- meta_efa/restapi/views.py
+++ meta_efa/restapi/views.py
@@ -2,8 +2,10 @@ from rest_framework import viewsets
 
 from main.models import Station
 from restapi.serializers import StationSerializer
-
+from rest_framework import filters
 
 class StationViewSet(viewsets.ModelViewSet):
     queryset = Station.objects.all()
     serializer_class = StationSerializer
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('full_name',)

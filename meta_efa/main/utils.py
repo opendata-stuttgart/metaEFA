82e5b9ca1e54105d209d08626f550e35c3cddd38
momo <github@mail.hackathon.de>
2015-05-16 15:56:54 +0200

    get efa data

--- meta_efa/main/utils.py
+++ meta_efa/main/utils.py
@@ -1,4 +1,6 @@
 import logging
+import time
+import requests
 
 from main.models import Station
 import csv
@@ -22,3 +24,59 @@ def populate_stations(path='/opt/code/vvs_data/HaltestellenVVS_simplified_utf8_s
                 logging.info("Created station %s" % row[0])
             else:
                 logging.info("Updated station %s" % row[0])
+
+
+def get_EFA_from_VVS(station_id):
+    """
+    send HTTP Request to VVS and return a xml string
+    """
+    # parameters needed for EFA
+    zocationServerActive = 1
+    lsShowTrainsExplicit = 1
+    stateless = 1
+    language = 'de'
+    SpEncId = 0
+    anySigWhenPerfectNoOtherMatches = 1
+    # max amount of arrivals to be returned
+    limit = 5
+    depArr = 'departure'
+    type_dm = 'any'
+    anyObjFilter_dm = 2
+    deleteAssignedStops = 1
+    name_dm = station_id
+    mode = 'direct'
+    dmLineSelectionAll = 1
+    itdDateYear = int(time.strftime('%y'))
+    itdDateMonth = int(time.strftime('%m'))
+    itdDateDay = int(time.strftime('%d'))
+    itdTimeHour = int(time.strftime('%H'))
+    itdTimeMinute = int(time.strftime('%M'))
+    useRealtime = 1
+    outputFormat = 'json'
+
+    url = 'http://www2.vvs.de/vvs/widget/XML_DM_REQUEST?'
+    url += 'zocationServerActive=%d' % zocationServerActive
+    url += '&lsShowTrainsExplicit%d' % lsShowTrainsExplicit
+    url += '&stateless=%d' % stateless
+    url += '&language=%s' % language
+    url += '&SpEncId=%d' % SpEncId
+    url += '&anySigWhenPerfectNoOtherMatches=%d'\
+        % anySigWhenPerfectNoOtherMatches
+    url += '&limit=%d' % limit
+    url += '&depArr=%s' % depArr
+    url += '&type_dm=%s' % type_dm
+    url += '&anyObjFilter_dm=%d' % anyObjFilter_dm
+    url += '&deleteAssignedStops=%d' % deleteAssignedStops
+    url += '&name_dm=%s' % name_dm
+    url += '&mode=%s' % mode
+    url += '&dmLineSelectionAll=%d' % dmLineSelectionAll
+    url += '&itdDateYear=%d' % itdDateYear
+    url += '&itdDateMonth=%d' % itdDateMonth
+    url += '&itdDateDay=%d' % itdDateDay
+    url += '&itdTimeHour=%d' % itdTimeHour
+    url += '&itdTimeMinute=%d' % itdTimeMinute
+    url += '&useRealtime=%d' % useRealtime
+    url += '&outputFormat=%s' % outputFormat
+
+    efa = requests.get(url).json()
+    return efa
--- meta_efa/restapi/views.py
+++ meta_efa/restapi/views.py
@@ -1,11 +1,22 @@
 from rest_framework import viewsets
-
+from rest_framework.decorators import detail_route
+from rest_framework.response import Response
 from main.models import Station
 from restapi.serializers import StationSerializer
 from rest_framework import filters
+from main.utils import get_EFA_from_VVS
+
 
 class StationViewSet(viewsets.ModelViewSet):
     queryset = Station.objects.all()
     serializer_class = StationSerializer
     filter_backends = (filters.SearchFilter,)
     search_fields = ('full_name',)
+
+    @detail_route()
+    def departures(self, request, **kwargs):
+        station = self.get_object()
+        station_id = station.station_id
+        get_EFA_from_VVS(station_id)
+
+        return Response(station_id)

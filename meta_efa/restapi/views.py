b4aed93485e2e08cfdcf7c21aab559cf1c34ea1d
Arne Schauf <git@as-webservices.de>
2015-05-16 19:15:04 +0200

    only search name

--- meta_efa/restapi/views.py
+++ meta_efa/restapi/views.py
@@ -11,7 +11,7 @@ class StationViewSet(viewsets.ModelViewSet):
     queryset = Station.objects.all()
     serializer_class = StationSerializer
     filter_backends = (filters.SearchFilter,)
-    search_fields = ('full_name',)
+    search_fields = ('name',)
 
     @detail_route()
     def departures(self, request, **kwargs):

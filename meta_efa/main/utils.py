70cba3ed5c1765c50011972152ceb460dc41d2f5
Arne Schauf <git@as-webservices.de>
2015-05-17 00:12:14 +0200

    use defaults for update_or_create

--- meta_efa/main/utils.py
+++ meta_efa/main/utils.py
@@ -16,9 +16,11 @@ def populate_stations(path='/opt/code/vvs_data/HaltestellenVVS_simplified_utf8_s
         next(reader, None)
         for row in reader:
             obj, created = Station.objects.update_or_create(
-                station_id=row[0],
-                name=row[1],
-                full_name=row[2])
+                station_id=row[0], defaults={
+                    'name': row[1],
+                    'full_name': row[2]
+                }
+            )
 
             if created:
                 logging.info("Created station %s" % row[0])

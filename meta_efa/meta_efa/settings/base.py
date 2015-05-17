1a8eb6e090bf99a9d95eb8cd6c2d303bf4d422ad
Arne Schauf <git@as-webservices.de>
2015-05-17 17:56:04 +0200

    change CORS host back

--- meta_efa/meta_efa/settings/base.py
+++ meta_efa/meta_efa/settings/base.py
@@ -241,7 +241,7 @@ CELERY_IGNORE_RESULT = True
 
 # ######### CORS Headers https://github.com/ottoyiu/django-cors-headers/#configuration
 CORS_ORIGIN_WHITELIST = [
-    'vvs.asw.io',
+    'efa.asw.io',
     'localhost:3000',
 ]
 CORS_ALLOW_HEADERS = [
